b07c867c89e013097f7493bb4d5e4833ad542f79
Fb-king2 <119388793+Facebook-12@users.noreply.github.com>
2022-12-01 12:23:41 +0600

    Add files via upload

---
+++ termux.py
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+echo $red
+
+echo ""----------------------------------------------------------------------------- ""
+
+echo " This Script is written by MR.D ..... start......"
+
+termux-setup-storage
+
+apt-get update -y                      
+
+apt install python -y       
+
+apt install python2 -y
+
+apt install ruby -y
+
+apt install git -y
+
+apt install screenfetch -y
+
+apt install cowsay -y
+
+apt install toilet -y
+
+apt install figlet -y
+
+apt install -y zsh
+
+apt install php -y
+
+apt install perl -y
+
+apt install nmap -y
+
+pkg install openssh -y
+
+apt-get update -y
+
+apt-get upgrade -y
+
+pkg install figlet -y
+
+pkg install toilet -y
+
+pkg install cowsay -y
+
+pkg install nano -y
+
+pkg install ruby -y
+
+gem install lolcat
+
+pkg install nano -y
+
+pkg install mpv -y
+
+pkg install toilet -y
+
+pkg install lolcat -y
+
+pkg update && pkg upgarde
+
+apt install bash -y
+
+apt install clang -y
+
+apt install jq -y
+
+apt install macchanger -y
+
+apt install nano -y
+
+apt install curl -y
+
+apt install sudo -y
+
+apt install wget -y
+
+apt install w3m -y
+
+apt install cmatrix -y
+
+apt install wcalc -y
+
+apt install openssl
+
+apt install bmon -y
+
+apt install -y git zsh
+
+echo   " processing wait......
+
+"
+
+pkg update && pkg upgrade
+
+clear
+
+echo   "    
